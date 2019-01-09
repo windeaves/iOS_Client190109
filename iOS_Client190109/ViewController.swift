6b889b0c33a2ca74405be998005ef1aa0320e35d
TonyTang2001 <tonytangdev@outlook.com>
2019-01-09 19:52:44 +0800

    Initial Commit

---
+++ iOS_Client190109/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS_Client190109
+//
+//  Created by 唐子轩 on 2019/1/9.
+//  Copyright © 2019 TonyTang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
